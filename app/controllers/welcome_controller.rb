647d1a2a50d5749d22e24d8bef5a70b7b59f2595
Delmar Almeida <delmar@Administrators-MacBook-Pro.local>
2018-11-05 19:24:09 +0000

    added pagination

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,12 @@
 class WelcomeController < ApplicationController
   def index
-    @projects = Project.all
-    @articles = Article.all
+    @articles = pages_for(Article.all.order("created_at desc"))
+    @projects = pages_for(Project.all.order("created_at desc"))
+  end
+
+  private
+
+  def pages_for(object)
+  	object.paginate(page: params[:page], per_page: 1)
   end
 end
